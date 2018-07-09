328a49f6ffd4ef00a518bc4e40272e8b97f33725
NoxCaos <glebx95@gmail.com>
2018-07-08 21:01:04 -0400

    Added Quaternion and angle tools

---
+++ Quaternion/QuaternionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Euclase.EQuaternion {
+    public static class QuaternionExtensions {
+
+        public static Quaternion Lerp(this Quaternion source, Quaternion target, float speed) {
+            return Quaternion.Lerp(source, target, Time.deltaTime * speed);
+        }
+
+        public static Quaternion Slerp(this Quaternion source, Quaternion target, float speed) {
+            return Quaternion.Slerp(source, target, Time.deltaTime * speed);
+        }
+
+    }
+}
--- Transform/TransformExtensions.cs
+++ Transform/TransformExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Euclase.EVector3;
+﻿using Euclase.EQuaternion;
+using Euclase.EVector3;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,16 @@ namespace Euclase.ETransform {
 
         public static Transform SetPos(this Transform tr, Vector3 pos) {
             tr.position = pos;
+            return tr;
+        }
+
+        public static Transform SetRot(this Transform tr, Quaternion rot) {
+            tr.rotation = rot;
+            return tr;
+        }
+
+        public static Transform SetLocalRot(this Transform tr, Quaternion rot) {
+            tr.localRotation = rot;
             return tr;
         }
 
@@ -23,11 +34,34 @@ namespace Euclase.ETransform {
             return tr.SetLocalPos(tr.localPosition.Lerp(target, speed));
         }
 
-        public static void ClearChildren(this Transform item) {
+        public static Transform LerpRot(this Transform tr, Quaternion target, float speed) {
+            return tr.SetRot(tr.rotation.Slerp(target, speed));
+        }
+
+        public static Transform LerpLocalRot(this Transform tr, Quaternion target, float speed) {
+            return tr.SetLocalRot(tr.localRotation.Slerp(target, speed));
+        }
+
+        public static Transform LerpLookAt(this Transform tr, Vector3 target, float speed) {
+            var look = Quaternion.LookRotation(target - tr.position);
+            return tr.SetRot(Quaternion.Slerp(tr.rotation, look, speed));
+        }
+
+        public static Transform LerpLookAt(this Transform tr, Vector3 target, Vector3 axis, float speed) {
+            var look = Quaternion.LookRotation(target - tr.position);
+            look.eulerAngles = new Vector3(
+                axis.x * look.eulerAngles.x,
+                axis.y * look.eulerAngles.y,
+                axis.z * look.eulerAngles.z);
+            return tr.LerpRot(look, speed);
+        }
+
+        public static Transform ClearChildren(this Transform item) {
             var children = new List<GameObject>();
             foreach(Transform child in item)
                 children.Add(child.gameObject);
             children.ForEach(child => Object.Destroy(child));
+            return item;
         }
 
     }
